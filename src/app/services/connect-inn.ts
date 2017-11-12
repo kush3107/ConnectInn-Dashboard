8865798ddf8c67e80c9504ddb3b0abc045c6c665
Kushagra <kkss420@gmail.com>
2017-11-12 23:30:22 +0530

    Added services and some very basic setup

---
+++ src/app/services/connect-inn.ts
@@ -0,0 +1,62 @@
+import {Http, RequestOptions, Headers} from "@angular/http";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Utils} from "../utils";
+import {MatSnackBar} from "@angular/material";
+
+export class ConnectInnService {
+  private BASE_URL = environment.apiBaseURL;
+
+  static hasLoginToken(): boolean {
+    return !!localStorage.getItem('auth_token');
+  }
+
+  constructor(private http: Http, private httpClient: HttpClient, private snackBar: MatSnackBar) {
+  }
+
+  private get(url: string, data?: any) {
+
+    const options = this.buildRequestOptions();
+
+    if (data) {
+      options.params = Utils.objToSearchParams(data);
+    }
+
+    return this.http.get(this.BASE_URL + url, options);
+  }
+
+  private post(url: string, data?: any) {
+
+    const options = this.buildRequestOptions();
+
+    return this.http.post(this.BASE_URL + url, data, options);
+  }
+
+  private put(url: string, data?: any) {
+
+    const options = this.buildRequestOptions();
+
+    return this.http.put(this.BASE_URL + url, data, options);
+  }
+
+  private delete(url: string, data?: any) {
+
+    const options = this.buildRequestOptions();
+    if (data) {
+      options.params = Utils.objToSearchParams(data);
+    }
+
+    return this.http.delete(this.BASE_URL + url, options);
+  }
+
+  private buildRequestOptions(): RequestOptions {
+    const options = new RequestOptions({headers: new Headers()});
+
+    const authToken = localStorage.getItem('auth_token');
+    if (authToken) {
+      options.headers.append('Authorization', 'bearer ' + authToken);
+    }
+
+    return options;
+  }
+}
---
+++ src/app/utils.ts
@@ -0,0 +1,83 @@
+import {URLSearchParams} from '@angular/http';
+
+export class Utils {
+  static objToSearchParams(obj): URLSearchParams {
+    const params: URLSearchParams = new URLSearchParams();
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        params.set(key, obj[key]);
+      }
+    }
+    return params;
+  }
+
+  static removeKey(obj, deleteKey) {
+    return Object.keys(obj)
+      .filter(key => key !== deleteKey)
+      .reduce((result, current) => {
+        result[current] = obj[current];
+        return result;
+      }, {});
+  }
+
+  static normalize(entityArray: Entity[]) {
+    const result = {};
+    for (let i = 0; i < entityArray.length; i++) {
+      result[entityArray[i].id] = entityArray[i];
+    }
+
+    return result;
+  }
+
+  static normalizedObjToArray(object: { [id: number]: any }) {
+    const result = [];
+    for (let i = 1; i <= Object.keys(object).length; i++) {
+      result.push(object[i]);
+    }
+
+    return result;
+  }
+
+  static getObjectValues(object: { [id: number]: any }) {
+    const vals = [];
+    for (const key in object) {
+      if (object.hasOwnProperty(key)) {
+        vals.push(object[key]);
+      }
+    }
+    return vals;
+  }
+
+  static removeNullFields(data: Object) {
+    Object.keys(data).forEach((key) => (data[key] == null) && delete data[key]);
+    return data;
+  }
+
+  static objectIsEquivalent(obj1, obj2) {
+    // Create arrays of key names
+    const obj1keys = Object.keys(obj1);
+    const obj2keys = Object.keys(obj2);
+
+    // If number of properties is different,
+    // objects are not equivalent
+    if (obj1keys.length !== obj2keys.length) {
+      return false;
+    }
+
+    for (let i = 0; i < obj1keys.length; i++) {
+      const keyName = obj1keys[i];
+      // If values of same property are not equal,
+      // objects are not equivalent
+      if (obj1[keyName] !== obj2[keyName]) {
+        return false;
+      }
+    }
+    // If we made it this far, objects
+    // are considered equivalent
+    return true;
+  }
+}
+
+interface Entity {
+  id: number;
+}
