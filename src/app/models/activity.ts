f6021b4f72b7cab6e826c42845602cbb6f77ec45
Kushagra <kkss420@gmail.com>
2017-11-12 23:13:05 +0530

    added models for now

---
+++ src/app/models/activity.ts
@@ -0,0 +1,12 @@
+export class Activity {
+  id: number;
+  title: string;
+  description: string;
+  start: string;
+  end: string;
+  type: string;
+  link: string;
+
+  createdAt: string;
+  udpatedAt: string;
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,13 @@
+export class User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  profile_pic: string;
+  about: string;
+  rating: number;
+  date_of_birth: string;
+
+  createdAt: string;
+  udpatedAt: string;
+}
