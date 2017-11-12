c75f524cc912b9d5660bc56e6f3c1a03ca28b489
Kushagra <kkss420@gmail.com>
2017-11-13 01:57:34 +0530

    added login service method

--- src/app/services/connect-inn.ts
+++ src/app/services/connect-inn.ts
@@ -4,6 +4,13 @@ import {environment} from "../../environments/environment";
 import {Utils} from "../utils";
 import {MatSnackBar} from "@angular/material";
 import {Injectable} from "@angular/core";
+import {Observable} from "rxjs/Observable";
+import {User} from "../models/user";
+import 'rxjs';
+import {isUndefined} from "util";
+import {Store} from "@ngrx/store";
+import {State} from "../reducers/index";
+import {LoginRequestAction, LoginSuccessAction} from "../actions/user";
 
 @Injectable()
 export class ConnectInnService {
@@ -13,7 +20,7 @@ export class ConnectInnService {
     return !!localStorage.getItem('auth_token');
   }
 
-  constructor(private http: Http, private httpClient: HttpClient, private snackBar: MatSnackBar) {
+  constructor(private http: Http, private httpClient: HttpClient, private snackBar: MatSnackBar, private store: Store<State>) {
   }
 
   private get(url: string, data?: any) {
@@ -61,4 +68,53 @@ export class ConnectInnService {
 
     return options;
   }
+
+  private handleError(error: Response): Observable<Error> {
+    console.log('[sd-error] start');
+    console.log(error);
+    console.log('[sd-error] end');
+
+    if (error.status === 401) {
+      localStorage.removeItem('auth_token');
+      this.snackBar.open('Unauthorized', '', {
+        duration: 4000,
+      });
+      return Observable.throw({messages: ['Unauthorized'], error: null});
+    }
+
+    const errorObject = error.json();
+    const errorResponse = errorObject['errors'];
+
+    const messages = [];
+
+    if (isUndefined(errorResponse)) {
+      messages.push(errorObject['message']);
+    } else {
+      if (typeof errorResponse === 'string') {
+        return Observable.throw({messages: [errorResponse], error: null});
+      } else {
+        for (const key in errorResponse) {
+          if (errorResponse.hasOwnProperty(key)) {
+            messages.push(errorResponse[key]);
+          }
+        }
+      }
+    }
+  }
+
+  login(data: { email: string, password: string }): Observable<User> {
+    this.store.dispatch(new LoginRequestAction());
+    return this.get('/authenticate', data).map(res => {
+      localStorage.setItem('auth_token', res.json().token);
+      const userObject = Object.assign(new User(), res.json().user);
+      this.store.dispatch(new LoginSuccessAction(userObject));
+      return userObject;
+    }).catch(err => this.handleError(err));
+  }
+
+  // register(data: {email: string, name: string, password: string, password_confirmation: string}): Observable<User> {
+  //   this.post('/register', data).map(res => {
+  //     const user =
+  //   });
+  // }
 }
