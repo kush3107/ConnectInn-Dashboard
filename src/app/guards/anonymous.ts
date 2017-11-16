ebd45b80b1d05dcd87ba37fcd11670cbfd955e9e
Kushagra <kkss420@gmail.com>
2017-11-17 03:23:43 +0530

    minor fix for home url

--- src/app/guards/anonymous.ts
+++ src/app/guards/anonymous.ts
@@ -11,7 +11,7 @@ export class AnonymousGuard implements CanActivate {
       return true;
     }
 
-    this.router.navigate(['/home']);
+    this.router.navigate(['/feed']);
     return false;
   }
 }
