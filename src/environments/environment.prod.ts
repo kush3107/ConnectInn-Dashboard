82ee7098b8c7bb7f422299cceecc2804aa5f3021
Kushagra <kkss420@gmail.com>
2017-12-13 19:48:50 +0530

    added prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+
+  apiBaseURL: 'http://api.connectinn.tk/api',
+  firebase: {
+    apiKey: 'AIzaSyDaB5pdNpfHr27qrX3msOHcaRihNWmXbxQ',
+    databaseURL: 'https://connectinn-tk.firebaseio.com/',
+    projectId: 'connectinn-tk',
+  }
 };
